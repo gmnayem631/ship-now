6727b2256961a4f0de01127047a9b702faed274a
gmnayem631 <gmnayem631@gmail.com>
2026-07-26 15:00:43 +0600

    feat: add UserProfileCard component and integrate it into Sidebar

---
+++ app/components/UserProfileCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { ChevronDown } from "lucide-react";
+
+const UserProfileCard = () => {
+  return (
+    <div className="group mt-3 flex w-full items-center gap-3 rounded-lg border border-transparent bg-[#f0f0f0] p-2">
+      {/* Avatar Image */}
+      <div className="relative h-10 w-10 shrink-0 overflow-hidden rounded-full">
+        <Image
+          src="/images/Avatar.png"
+          alt="John Doe"
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      {/* User Info */}
+      <div className="flex flex-1 flex-col items-start">
+        <h3 className="text-base font-semibold text-[#333]">John Doe</h3>
+        <p className="text-sm font-normal text-[#757575]">Admin</p>
+      </div>
+
+      {/* Chevron Icon */}
+      <ChevronDown className="h-5 w-5 shrink-0 text-gray-800" />
+    </div>
+  );
+};
+
+export default UserProfileCard;
--- app/components/ui/Sidebar.tsx
+++ app/components/ui/Sidebar.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import ShipNowLogo from "../ShipNowLogo";
+import UserProfileCard from "../UserProfileCard";
 
 const Sidebar = () => {
   return (
-    <div className="m-4 p-3 text-center font-bold text-black">Sidebar</div>
+    <div className="h-screen w-55.75 border-2 bg-[#fefefe] px-4 py-5 text-center font-bold text-black">
+      <div className="flex items-center gap-2 text-xl font-black text-[#333] italic">
+        <ShipNowLogo />
+        <span className="uppercase">ShipNow</span>
+      </div>
+
+      {/* User */}
+      <UserProfileCard />
+    </div>
   );
 };
 
