53fe641b598465d61cb8b5f82ddb33ca8b12176d
gmnayem631 <gmnayem631@gmail.com>
2026-07-28 16:41:56 +0600

    feat: add company logos as components

---
+++ app/components/company-logos/EcoLights.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const EcoLights = () => {
+  return (
+    <svg
+      width="36"
+      height="36"
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.8796 5.76001H27.0584L15.899 22.3005H0.720215L11.8796 5.76001Z"
+        fill="#333333"
+      />
+      <path
+        d="M11.8796 5.76001H27.0584L15.899 22.3005H0.720215L11.8796 5.76001Z"
+        fill="black"
+        fill-opacity="0.2"
+      />
+      <path
+        d="M12.5445 23.8335L8.22217 30.24H24.1209L35.2803 13.6995H23.5461L16.709 23.8335H12.5445Z"
+        fill="#856DF3"
+      />
+    </svg>
+  );
+};
+
+export default EcoLights;
---
+++ app/components/company-logos/QuickParts.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const QuickParts = () => {
+  return (
+    <svg
+      width="36"
+      height="36"
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1762_4932)">
+        <path
+          d="M25.7143 6.47998H36L27.1169 29.52H16.8312L25.7143 6.47998Z"
+          fill="#F04A4A"
+        />
+        <path
+          d="M16.3636 6.47998H23.8442L14.961 29.52H7.48052L16.3636 6.47998Z"
+          fill="#F04A4A"
+        />
+        <path
+          d="M8.88312 6.47998H14.4935L5.61039 29.52H0L8.88312 6.47998Z"
+          fill="#856DF3"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1762_4932">
+          <rect width="36" height="36" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default QuickParts;
